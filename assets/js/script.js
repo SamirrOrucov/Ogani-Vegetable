8e4a8a47bbd2d1c926763b59f270d74c64b751d5
SamirrOrucov <147073824+SamirrOrucov@users.noreply.github.com>
2023-11-16 20:03:31 +0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const button=document.querySelector(".allDepartments .allDepartments_head")
+const div=document.querySelector(".allDepartments .lists")
+
+
+button.addEventListener("click",function () {
+    div.classList.toggle("show")
+    
+})
+
